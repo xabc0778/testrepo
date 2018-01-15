1ac539d83a0bdf700682251deda00937a66de221
xabc0778 <xabc0778@gmail.com>
2018-01-14 21:41:11 -0500

    First test project commit

---
+++ src/test/java/com/qa/test/LoginTest.java
@@ -0,0 +1,14 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+
+	
+	@Test
+	public void logintest(){
+		System.out.println("This is a login test method");
+	}
+	
+	
+}
